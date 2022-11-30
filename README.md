42a73842c8be2b878e7b328f901566eb61b4429a
Bill <Bill@TheWallisFamily.co.uk>
2022-11-30 17:24:48 +0000

    Rename the core table from `dates` to `calendar`

---
+++ README.md
@@ -0,0 +1,16 @@
+sql-schema-models
+---
+
+Various files to create stand-alone SQL schemas that can be utilised in larger data models.
+
+
+# Date Schema
+
+Almost all data models can be improved by adding a calendar table which has dates over a range with useful info and such, such as weekday names.
+
+This model extends this by including holidays and additional columns.
+
+
+# Geography Schema
+
+Countries and cities.
--- date-schema/main.py
+++ date-schema/main.py
@@ -1,12 +1,18 @@
 import pandas as pd
-import sqlalchemy.engine as sql_e
+import sqlalchemy.engine
 
 from utils.date_dimension import DateDimension
 from utils.bank_holidays import BankHolidays
 
 
-def write_to_database(dataframe: pd.DataFrame, table_name: str, sqlalchemy_engine: sql_e.Engine) -> None:
-    """This needs to be significantly refactored -- need to sanitise the parameters."""
+def write_to_database(
+    dataframe: pd.DataFrame,
+    table_name: str,
+    sqlalchemy_engine: sqlalchemy.engine.Engine
+) -> None:
+    """
+    This needs to be significantly refactored -- need to sanitise the parameters.
+    """
     sqlalchemy_engine.execute(statement=f"""DELETE FROM {table_name} WHERE True;""")
     dataframe.to_sql(
         name=table_name,
@@ -17,11 +23,11 @@ def write_to_database(dataframe: pd.DataFrame, table_name: str, sqlalchemy_engin
 
 
 def main():
-    engine = sql_e.create_engine('sqlite:///date-schema.db')
+    engine = sqlalchemy.engine.create_engine('sqlite:///date-schema.db')
     date_dim = DateDimension(start='1980-01-01', end='2079-12-31')
     bank_hols = BankHolidays()
 
-    write_to_database(date_dim.date_frame, 'dates', engine)
+    write_to_database(date_dim.date_frame, 'calendar', engine)
     write_to_database(bank_hols.bank_holiday_frame, 'bank_holidays', engine)
 
 
